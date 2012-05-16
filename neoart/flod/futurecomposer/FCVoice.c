97bdee521c79da841d69b894e4493579bb2dab6e
rofl0r <retnyg@gmx.net>
2012-05-17 00:21:11 +0200

    hopefully complete conversion of FCVoice

--- neoart/flod/futurecomposer/FCVoice.c
+++ neoart/flod/futurecomposer/FCVoice.c
@@ -15,92 +15,37 @@
   To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/3.0/ or send a letter to
   Creative Commons, 171 Second Street, Suite 300, San Francisco, California, 94105, USA.
 */
-package neoart->flod->futurecomposer {
-  import neoart.flod.core.*;
 
-  public final class FCVoice {
-    internal var
- int index;
-      next           : FCVoice,
-      channel        : AmigaChannel,
-      sample         : AmigaSample,
- int enabled;
- int pattern;
- int soundTranspose;
- int transpose;
- int patStep;
- int frqStep;
- int frqPos;
- int frqSustain;
- int frqTranspose;
- int volStep;
- int volPos;
- int volCtr;
- int volSpeed;
- int volSustain;
- int note;
- int pitch;
- int volume;
- int pitchBendFlag;
- int pitchBendSpeed;
- int pitchBendTime;
- int portamentoFlag;
- int portamento;
- int volBendFlag;
- int volBendSpeed;
- int volBendTime;
- int vibratoFlag;
- int vibratoSpeed;
- int vibratoDepth;
- int vibratoDelay;
- int vibrato;
+#include "FCVoice.h"
+#include "../flod_internal.h"
 
-     void FCVoice( int index) {
-      self->index = index;
-    }
+void FCVoice_defaults(struct FCVoice* self) {
+	CLASS_DEF_INIT();
+	// static initializers go here
+}
 
-    internal function initialize():void {
-      sample         = null;
-      enabled        = 0;
-      pattern        = 0;
-      soundTranspose = 0;
-      transpose      = 0;
-      patStep        = 0;
-      frqStep        = 0;
-      frqPos         = 0;
-      frqSustain     = 0;
-      frqTranspose   = 0;
-      volStep        = 0;
-      volPos         = 0;
-      volCtr         = 1;
-      volSpeed       = 1;
-      volSustain     = 0;
-      note           = 0;
-      pitch          = 0;
-      volume         = 0;
-      pitchBendFlag  = 0;
-      pitchBendSpeed = 0;
-      pitchBendTime  = 0;
-      portamentoFlag = 0;
-      portamento     = 0;
-      volBendFlag    = 0;
-      volBendSpeed   = 0;
-      volBendTime    = 0;
-      vibratoFlag    = 0;
-      vibratoSpeed   = 0;
-      vibratoDepth   = 0;
-      vibratoDelay   = 0;
-      vibrato        = 0;
-    }
+void FCVoice_ctor(struct FCVoice* self) {
+	CLASS_CTOR_DEF(FCVoice);
+	// original constructor code goes here
+	self->index = index;
+}
 
-    internal function volumeBend():void {
-      volBendFlag ^= 1;
+struct FCVoice* FCVoice_new(int index) {
+	CLASS_NEW_BODY(FCVoice, index);
+}
 
-      if (volBendFlag) {
-        volBendTime--;
-        volume += volBendSpeed;
-        if (volume < 0 || volume > 64) volBendTime = 0;
-      }
-    }
-  }
+void FCVoice_initialize(struct FCVoice* self) {
+	CLASS_DEF_INIT(); // zeromem
+	self->volCtr         = 1;
+	self->volSpeed       = 1;
+}
+
+void FCVoice_volumeBend(struct FCVoice* self) {
+	self->volBendFlag ^= 1;
+
+	if (self->volBendFlag) {
+		self->volBendTime--;
+		self->volume += self->volBendSpeed;
+		if (self->volume < 0 || self->volume > 64) self->volBendTime = 0;
+	}
 }
