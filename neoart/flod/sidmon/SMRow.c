558b920a13994a16b862d6803e9321f484d2bfa7
rofl0r <retnyg@gmx.net>
2012-05-26 06:55:50 +0200

    SMRow

--- neoart/flod/sidmon/SMRow.c
+++ neoart/flod/sidmon/SMRow.c
@@ -15,11 +15,20 @@
   To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/3.0/ or send a letter to
   Creative Commons, 171 Second Street, Suite 300, San Francisco, California, 94105, USA.
 */
-package neoart->flod->sidmon {
-  import neoart.flod.core.*;
+#include "SMRow.h"
+#include "../flod_internal.h"
 
-  public final class SMRow extends AmigaRow {
-    internal var
- int speed;
-  }
+void SMRow_defaults(struct SMRow* self) {
+	CLASS_DEF_INIT();
+	// static initializers go here
 }
+
+void SMRow_ctor(struct SMRow* self) {
+	CLASS_CTOR_DEF(SMRow);
+	// original constructor code goes here
+}
+
+struct SMRow* SMRow_new(void) {
+	CLASS_NEW_BODY(SMRow);
+}
+
