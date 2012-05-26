2b51c0ae1c138094ac849a548f49b575a46971ae
rofl0r <retnyg@gmx.net>
2012-05-26 02:15:55 +0200

    DMSample

--- neoart/flod/digitalmugician/DMSample.c
+++ neoart/flod/digitalmugician/DMSample.c
@@ -15,26 +15,22 @@
   To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/3.0/ or send a letter to
   Creative Commons, 171 Second Street, Suite 300, San Francisco, California, 94105, USA.
 */
-package neoart->flod->digitalmugician {
-  import neoart.flod.core.*;
 
-  public final class DMSample extends AmigaSample {
-    internal var
- int wave;
- int waveLen;
- int finetune;
- int arpeggio;
- int pitch;
- int pitchDelay;
- int pitchLoop;
- int pitchSpeed;
- int effect;
- int effectDone;
- int effectStep;
- int effectSpeed;
- int source1;
- int source2;
- int volumeLoop;
- int volumeSpeed;
-  }
+#include "DMSample.h"
+#include "../flod_internal.h"
+
+void DMSample_defaults(struct DMSample* self) {
+	CLASS_DEF_INIT();
+	// static initializers go here
 }
+
+void DMSample_ctor(struct DMSample* self) {
+	CLASS_CTOR_DEF(DMSample);
+	// original constructor code goes here
+	AmigaSample_defaults(&self->super);
+}
+
+struct DMSample* DMSample_new(void) {
+	CLASS_NEW_BODY(DMSample);
+}
+
