658ffca679d7db07e4e78e6d54bd32a03e5f0752
rofl0r <retnyg@gmx.net>
2012-05-26 02:26:31 +0200

    DMVoice

--- neoart/flod/digitalmugician/DMVoice.c
+++ neoart/flod/digitalmugician/DMVoice.c
@@ -15,58 +15,45 @@
   To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/3.0/ or send a letter to
   Creative Commons, 171 Second Street, Suite 300, San Francisco, California, 94105, USA.
 */
-package neoart->flod->digitalmugician {
-  import neoart.flod.core.*;
 
-  public final class DMVoice {
-    internal var
-      channel      : AmigaChannel,
-      sample       : DMSample,
-      step         : AmigaStep,
- int note;
- int period;
- int val1;
- int val2;
- int finalPeriod;
- int arpeggioStep;
- int effectCtr;
- int pitch;
- int pitchCtr;
- int pitchStep;
- int portamento;
- int volume;
- int volumeCtr;
- int volumeStep;
- int mixMute;
- int mixPtr;
- int mixEnd;
- int mixSpeed;
- int mixStep;
- int mixVolume;
+#include "DMVoice.h"
+#include "../flod_internal.h"
 
-    internal function initialize():void {
-      sample       = null;
-      step         = null;
-      note         = 0;
-      period       = 0;
-      val1         = 0;
-      val2         = 0;
-      finalPeriod  = 0;
-      arpeggioStep = 0;
-      effectCtr    = 0;
-      pitch        = 0;
-      pitchCtr     = 0;
-      pitchStep    = 0;
-      portamento   = 0;
-      volume       = 0;
-      volumeCtr    = 0;
-      volumeStep   = 0;
-      mixMute      = 1;
-      mixPtr       = 0;
-      mixEnd       = 0;
-      mixSpeed     = 0;
-      mixStep      = 0;
-      mixVolume    = 0;
-    }
-  }
+void DMVoice_defaults(struct DMVoice* self) {
+	CLASS_DEF_INIT();
+	// static initializers go here
+}
+
+void DMVoice_ctor(struct DMVoice* self) {
+	CLASS_CTOR_DEF(DMVoice);
+	// original constructor code goes here
+}
+
+struct DMVoice* DMVoice_new(void) {
+	CLASS_NEW_BODY(DMVoice);
+}
+
+void DMVoice_initialize(struct DMVoice *self) {
+	self->sample       = NULL;
+	self->step         = NULL;
+	self->note         = 0;
+	self->period       = 0;
+	self->val1         = 0;
+	self->val2         = 0;
+	self->finalPeriod  = 0;
+	self->arpeggioStep = 0;
+	self->effectCtr    = 0;
+	self->pitch        = 0;
+	self->pitchCtr     = 0;
+	self->pitchStep    = 0;
+	self->portamento   = 0;
+	self->volume       = 0;
+	self->volumeCtr    = 0;
+	self->volumeStep   = 0;
+	self->mixMute      = 1;
+	self->mixPtr       = 0;
+	self->mixEnd       = 0;
+	self->mixSpeed     = 0;
+	self->mixStep      = 0;
+	self->mixVolume    = 0;
 }
