bc27eef7d83c7380a5b03cfce55c6fa1ab3cb345
rofl0r <retnyg@gmx.net>
2012-05-27 20:22:55 +0200

    RHSong

--- neoart/flod/hubbard/RHSong.c
+++ neoart/flod/hubbard/RHSong.c
@@ -1,25 +1,18 @@
-/*
-  Flod 4.1
-  2012/04/30
-  Christian Corti
-  Neoart Costa Rica
+#include "RHSong.h"
+#include "../flod_internal.h"
 
-  Last Update: Flod 4.0 - 2012/02/12
 
-  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
-  OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-  LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
-  IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
-  This work is licensed under the Creative Commons Attribution-Noncommercial-Share Alike 3.0 Unported License.
-  To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/3.0/ or send a letter to
-  Creative Commons, 171 Second Street, Suite 300, San Francisco, California, 94105, USA.
-*/
-package neoart->flod->hubbard {
-
-  public final class RHSong {
-    internal var
- int speed;
-      tracks : Vector.<int>;
-  }
+void RHSong_defaults(struct RHSong* self) {
+	CLASS_DEF_INIT();
+	// static initializers go here
 }
+
+void RHSong_ctor(struct RHSong* self) {
+	CLASS_CTOR_DEF(RHSong);
+	// original constructor code goes here
+}
+
+struct RHSong* RHSong_new(void) {
+	CLASS_NEW_BODY(RHSong);
+}
+
