46a758c7f9cc11653fb6829cc21a4f6c085a3c11
rofl0r <retnyg@gmx.net>
2013-06-04 23:22:18 +0200

    add SDL_audio backend

---
+++ backends/sdlwriter.h
@@ -0,0 +1,28 @@
+#ifndef SDLAUDIOWRITER_H
+#define SDLAUDIOWRITER_H
+
+#include <stddef.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <SDL/SDL_audio.h>
+
+#include "backend.h"
+
+struct SdlWriter {
+	struct Backend super;
+	pthread_mutex_t cb_lock;
+	char* buffer;
+	size_t buffer_used;
+	size_t buffer_size;
+	SDL_AudioSpec fmt;
+};
+
+int SdlWriter_init(struct SdlWriter *self, void* data);
+int SdlWriter_write(struct SdlWriter *self, void* buffer, size_t bufsize);
+int SdlWriter_close(struct SdlWriter *self);
+
+//RcB: DEP "sdlwriter.c"
+//RcB: LINK "-lSDL_sound"
+//RcB: LINK "-lpthread"
+
+#endif
